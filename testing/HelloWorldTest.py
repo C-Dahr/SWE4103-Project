6db97f7e45a77b6b77abcb024b4a49fd26109b1a
Maurice <mlavoie1@unb.ca>
2018-10-13 15:46:36 -0300

    Sample test scripts

--- testing/HelloWorldTest.py
+++ testing/HelloWorldTest.py
@@ -2,13 +2,37 @@ import requests
 
 # print("hello world")
 
-url = 'http://127.0.0.1:5000/HelloWorld'
+mainUrl = 'http://127.0.0.1:5000'
+
+url = mainUrl + '/HelloWorld'
+urlGameSched = mainUrl + '/api/game-schedule'
+urlTournSched = mainUrl + '/api/tournament-schedule'
+urlGameStats = mainUrl + '/api/game-stats/<game_id>'
+urlStandings = mainUrl + '/api/standings'
+urlPlayerStats = mainUrl + '/api/player-stats/<player_id>'
+urlRoster = mainUrl + '/api/roster/<team_id>'
+urlLogin = mainUrl + '/api/login'
+urlUserCreate = mainUrl + '/api/register'
+urlRoot = mainUrl + '/'
+
 urlUserCreate = 'http://127.0.0.1:5000/api/register'
+
 expectedStatus = 200
 # expectedText = {"hello": "world"}
 
 print(requests.get(url).status_code)
 assert requests.get(url).status_code == expectedStatus
 
-print(requests.get(urlUserCreate).status_code)
-assert requests.get(urlUserCreate).status_code == expectedStatus
+print(requests.post(urlUserCreate).status_code)
+assert requests.post(urlUserCreate).status_code == expectedStatus
+
+'''
+print(requests.post(urlUserCreate).status_code)
+assert requests.post(urlUserCreate).status_code == expectedStatus
+
+print(requests.post(urlUserCreate).status_code)
+assert requests.post(urlUserCreate).status_code == expectedStatus
+
+print(requests.post(urlUserCreate).status_code)
+assert requests.post(urlUserCreate).status_code == expectedStatus
+'''

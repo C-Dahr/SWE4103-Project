e57eaf78249f122a50c14f6dbb2d2abb0b81b9a0
Maurice <mlavoie1@unb.ca>
2018-10-13 15:27:13 -0300

    Sample test scripts

--- testing/HelloWorldTest.py
+++ testing/HelloWorldTest.py
@@ -3,8 +3,12 @@ import requests
 # print("hello world")
 
 url = 'http://127.0.0.1:5000/HelloWorld'
+urlUserCreate = 'http://127.0.0.1:5000/api/register'
 expectedStatus = 200
-expectedText = {"hello": "world"}
+# expectedText = {"hello": "world"}
 
 print(requests.get(url).status_code)
 assert requests.get(url).status_code == expectedStatus
+
+print(requests.get(urlUserCreate).status_code)
+assert requests.get(urlUserCreate).status_code == expectedStatus

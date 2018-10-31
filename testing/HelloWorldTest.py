f377af93d11cd3940707dfa0a6b8ec74599b90a4
Maurice <mlavoie1@unb.ca>
2018-10-31 17:11:07 -0300

    changes for testing

--- testing/HelloWorldTest.py
+++ testing/HelloWorldTest.py
@@ -5,11 +5,11 @@ import requests
 mainUrl = 'http://127.0.0.1:5000'
 
 url = mainUrl + '/HelloWorld'
-urlGameSched = mainUrl + '/api/game-schedule'
-urlTournSched = mainUrl + '/api/tournament-schedule'
+urlGameSchedule = mainUrl + '/api/game-schedule'
+urlTournamentSchedule = mainUrl + '/api/tournament-schedule'
 urlGameStats = mainUrl + '/api/game-stats/<game_id>'
-urlStandings = mainUrl + '/api/standings'
-urlPlayerStats = mainUrl + '/api/player-stats/<player_id>'
+urlLeagueStanding = mainUrl + '/api/standings'
+urlPlayerStats = mainUrl + '/api/player-stats/'
 urlRoster = mainUrl + '/api/roster/<team_id>'
 urlLogin = mainUrl + '/api/login'
 urlUserCreate = mainUrl + '/api/register'
@@ -17,17 +17,22 @@ urlRoot = mainUrl + '/'
 
 urlUserCreate = 'http://127.0.0.1:5000/api/register'
 
-expectedStatus = 200
+expectedStatus = 200 #and 201 for successes
 expectedText = {"hello": "world"}
-
-print(requests.get(url).status_code)
+print(url)
+print("HelloWorld:", requests.get(url).status_code)
 assert requests.get(url).status_code == expectedStatus
 r = requests.get(url).json()
-print(r)
+
+print("r =",r)
 assert r == expectedText
 
-print(requests.post(urlUserCreate).status_code)
-assert requests.post(urlUserCreate).status_code == expectedStatus
+#print("UserCreate:",requests.post(urlUserCreate).status_code)
+#assert requests.post(urlUserCreate).status_code == expectedStatus
+a = urlPlayerStats + "10"
+print(urlLeagueStanding)
+print("PlayerStats =",requests.get(urlLeagueStanding).status_code)
+#assert requests.get(urlPlayerStats, 10).status_code == expectedStatus
 
 '''
 print(requests.post(urlUserCreate).status_code)

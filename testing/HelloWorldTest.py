88caa9fc3a5278ff4808b093c6125def42052856
Maurice <mlavoie1@unb.ca>
2018-11-01 13:58:39 -0300

    created tests responses of league, team and player

--- testing/HelloWorldTest.py
+++ testing/HelloWorldTest.py
@@ -14,26 +14,60 @@ urlRoster = mainUrl + '/api/roster/<team_id>'
 urlLogin = mainUrl + '/api/login'
 urlUserCreate = mainUrl + '/api/register'
 urlRoot = mainUrl + '/'
-urlTeam = mainUrl + '/api/team'
 
 urlUserCreate = 'http://127.0.0.1:5000/api/register'
 
-expectedStatus = 200 #and 201 for successes
-expectedText = {"hello": "world"}
-print(url)
-print("HelloWorld:", requests.get(url).status_code)
-assert requests.get(url).status_code == expectedStatus
-r = requests.get(url).json()
-
-print("r =",r)
-assert r == expectedText
+#Tests
+urlLeague = mainUrl + '/api/league'
+urlTeam = mainUrl + '/api/team'
+urlPlayer = mainUrl + '/api/player'
 
 
-#Team
 
-#Player
 
 #League
+leagueCheck = True
+leagueGet_code = requests.get(urlLeague).status_code
+leaguePost_code = requests.post(urlLeague).status_code
+print("League - Get and Post")
+if leagueGet_code != 200:
+    print('League Get =', leagueGet_code)
+    leagueCheck = None
+if leaguePost_code != 201:
+    print('League Post =', leaguePost_code)
+    leagueCheck = None
+if leagueCheck:
+    print('success')
+
+#Team
+teamCheck = True
+teamGet_code = requests.get(urlTeam).status_code
+teamPost_code = requests.post(urlTeam).status_code
+print("Test - Get and Post")
+if teamGet_code != 200:
+    print('Team Get =', teamGet_code)
+    teamCheck = None
+if teamPost_code != 201:
+    print('Team Post =', teamPost_code)
+    teamCheck = None
+if teamCheck:
+    print('success')
+
+#Player
+playerCheck = True
+playerGet_code = requests.get(urlPlayer).status_code
+playerPost_code = requests.post(urlPlayer).status_code
+print("Player - Get and Post")
+if playerGet_code != 200:
+    print('Player Get =', playerGet_code)
+    playerCheck = None
+if playerPost_code != 201:
+    print('Player Post =', playerPost_code)
+    playerCheck = None
+if playerCheck:
+    print('success')
+
+
 
 
 #print("UserCreate:",requests.post(urlUserCreate).status_code)
@@ -54,4 +88,16 @@ assert requests.post(urlUserCreate).status_code == expectedStatus
 
 print(requests.post(urlUserCreate).status_code)
 assert requests.post(urlUserCreate).status_code == expectedStatus
+'''
+
+'''
+expectedStatus = 200 #and 201 for successes
+expectedText = {"hello": "world"}
+print(url)
+print("HelloWorld:", requests.get(url).status_code)
+assert requests.get(url).status_code == expectedStatus
+r = requests.get(url).json()
+
+print("r =",r)
+assert r == expectedText
 '''
